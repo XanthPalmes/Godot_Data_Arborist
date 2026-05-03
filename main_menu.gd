383cbb9c310255b13d5aa02e8f8bc7de14a405a9
Vaintseishi <xanthreign.palmes-25@cpu.edu.ph>
2026-05-03 15:17:06 +0800

    Added Main Menu
    Main menu scene with working buttons

--- main_menu.gd
+++ main_menu.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://ModeSelection.tscn")
+	
+
+
+func _on_exit_pressed():
+	get_tree().quit()
